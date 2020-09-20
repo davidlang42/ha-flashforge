1baa57ec3878b415569a9718a23dac5221c40590
David Lang <davidlang42@gmail.com>
2020-09-21 00:20:29 +1000

    Fix

--- custom_components/flashforge/parse.py
+++
@@ -1,8 +0,0 @@
-def parse_values(text):
-    lines = text.split('\r\n')
-    values = {}
-    for line in lines:
-        pair = line.split(':',1)
-        if len(pair) == 2:
-            values[pair[0]] = pair[1]
-    return values
--- custom_components/flashforge/sensor.py
+++ custom_components/flashforge/sensor.py
@@ -5,8 +5,13 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.const import CONF_HOST
 
 import socket
-import packets
-import parse
+
+REQUEST_CONTROL = '~M601 S1\r\n'
+REQUEST_INFO = '~M115\r\n'
+REQUEST_HEAD_POSITION = '~M114\r\n'
+REQUEST_TEMP = '~M105\r\n'
+REQUEST_PROGRESS = '~M27\r\n'
+REQUEST_STATUS = '~M119\r\n'
 
 BUFFER_SIZE = 1024
 TIMEOUT_SECONDS = 5
@@ -37,15 +42,15 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     sensor_name = config.get(CONF_NAME)
     printer_address = {'ip': config.get(CONF_HOST), 'port': config.get(CONF_PORT)}
-    request_data = [packets.request_control, packets.request_status]
+    request_data = [REQUEST_CONTROL, REQUEST_STATUS]
     if config.get(CONF_INCLUDE_INFO):
-        request_data.append(packets.request_info)
+        request_data.append(REQUEST_INFO)
     if config.get(CONF_INCLUDE_HEAD):
-        request_data.append(packets.request_head_position)
+        request_data.append(REQUEST_HEAD_POSITION)
     if config.get(CONF_INCLUDE_TEMP):
-        request_data.append(packets.request_temp)
+        request_data.append(REQUEST_TEMP)
     if config.get(CONF_INCLUDE_PROGRESS):
-        request_data.append(packets.request_progress)
+        request_data.append(REQUEST_PROGRESS)
 
     add_entities([FlashforgePrinter(sensor_name, printer_address, request_data)])
     return True
@@ -100,7 +105,7 @@ class FlashforgePrinter(Entity):
                 for message in self._request:
                     printer_socket.send(message.encode())
                     raw_data = printer_socket.recv(BUFFER_SIZE)
-                    data.update(parse.parse_values(raw_data.decode()))
+                    data.update(parse_values(raw_data.decode()))
                 printer_socket.shutdown(socket.SHUT_RDWR)
                 printer_socket.close()
         except:
@@ -109,3 +114,12 @@ class FlashforgePrinter(Entity):
             else:
                 data['error'] = 'Raw data: ' + raw_data.decode()
         self._data = data
+
+    def parse_values(text):
+        lines = text.split('\r\n')
+        values = {}
+        for line in lines:
+            pair = line.split(':',1)
+            if len(pair) == 2:
+                values[pair[0]] = pair[1]
+        return values
